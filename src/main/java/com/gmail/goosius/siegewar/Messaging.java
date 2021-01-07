3eb0ed9abdd945ff204c7db41871b4d8480d01de
Llm Dl <LlmDlio@gmail.com>
2021-01-06 19:01:55 -0600

    - Fix Messaging class having the wrong imports.

--- src/main/java/com/gmail/goosius/siegewar/Messaging.java
+++ src/main/java/com/gmail/goosius/siegewar/Messaging.java
@@ -1,19 +1,19 @@
 package com.gmail.goosius.siegewar;
 
-import org.bukkit.Color;
+import com.palmergames.bukkit.util.Colors;
 import org.bukkit.command.CommandSender;
 
-import com.palmergames.bukkit.towny.object.Translation;
+import com.gmail.goosius.siegewar.settings.Translation;
 
 public class Messaging {
 
 	final static String prefix = Translation.of("plugin_prefix");
 	
 	public static void sendErrMessage(CommandSender sender, String message) {
-		sender.sendMessage(prefix + Color.RED + message);
+		sender.sendMessage(prefix + Colors.Red + message);
 	}
 
 	public static void sendMessage(CommandSender sender, String message) {
-		sender.sendMessage(prefix + Color.WHITE + message);
+		sender.sendMessage(prefix + Colors.White + message);
 	}
 }
