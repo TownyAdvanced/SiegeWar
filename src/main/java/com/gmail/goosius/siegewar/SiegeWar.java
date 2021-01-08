c5d0af1f03e57bf9dfe0ea8b95ef2b8d812bb701
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2021-01-08 23:35:04 +0100

    Update SiegeWar.java

--- src/main/java/com/gmail/goosius/siegewar/SiegeWar.java
+++ src/main/java/com/gmail/goosius/siegewar/SiegeWar.java
@@ -28,12 +28,14 @@ public class SiegeWar extends JavaPlugin {
 	private static SiegeWar plugin;
 	public static String prefix = "[SiegeWar] ";
 	private static Version requiredTownyVersion = Version.fromString("0.96.5.12");
-	
+	private final static SiegeHUDManager SiegeHudManager = new SiegeHUDManager(plugin);
+
 	public static SiegeWar getSiegeWar() {
 		return plugin;
 	}
+
 	public static SiegeHUDManager getSiegeHUDManager() {
-		return new SiegeHUDManager(getSiegeWar());
+		return SiegeHudManager;
 	}
 	
     @Override
