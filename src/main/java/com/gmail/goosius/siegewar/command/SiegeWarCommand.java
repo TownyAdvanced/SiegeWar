629ed6cc9c00da0191322435efeea129debf908b
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2021-01-09 02:06:54 +0100

    Hud command auto completion and parsing

--- src/main/java/com/gmail/goosius/siegewar/command/SiegeWarCommand.java
+++ src/main/java/com/gmail/goosius/siegewar/command/SiegeWarCommand.java
@@ -1,5 +1,6 @@
 package com.gmail.goosius.siegewar.command;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,17 +10,22 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
+import com.gmail.goosius.siegewar.Messaging;
+import com.gmail.goosius.siegewar.SiegeController;
+import com.gmail.goosius.siegewar.SiegeWar;
 import com.gmail.goosius.siegewar.enums.SiegeWarPermissionNodes;
 import com.gmail.goosius.siegewar.settings.Translation;
 import com.gmail.goosius.siegewar.utils.SiegeWarMoneyUtil;
-
+import com.palmergames.bukkit.towny.TownyUniverse;
+import com.palmergames.bukkit.towny.exceptions.TownyException;
+import com.palmergames.bukkit.towny.object.Town;
 import com.palmergames.bukkit.towny.utils.NameUtil;
 import com.palmergames.bukkit.util.ChatTools;
 import com.palmergames.util.StringMgmt;
 
 public class SiegeWarCommand implements CommandExecutor, TabCompleter {
 	
-	private static final List<String> siegewarTabCompletes = Arrays.asList("nation");
+	private static final List<String> siegewarTabCompletes = Arrays.asList("nation", "hud");
 	
 	private static final List<String> siegewarNationTabCompletes = Arrays.asList("refund");
 	
@@ -27,8 +33,11 @@ public class SiegeWarCommand implements CommandExecutor, TabCompleter {
 
 		switch (args[0].toLowerCase()) {
 		case "nation":
-			return NameUtil.filterByStart(siegewarNationTabCompletes, args[1]);
-		
+			if (args.length > 1)
+				return NameUtil.filterByStart(siegewarNationTabCompletes, args[1]);
+		case "hud":
+			if (args.length > 1)
+				return NameUtil.filterByStart(new ArrayList<String>(SiegeController.getSiegedTownNames()), args[1]);
 		default:
 			return NameUtil.filterByStart(siegewarTabCompletes, args[0]);
 		}
@@ -40,6 +49,7 @@ public class SiegeWarCommand implements CommandExecutor, TabCompleter {
 		else {
 			sender.sendMessage(ChatTools.formatTitle("/siegewar"));
 			sender.sendMessage(ChatTools.formatCommand("Eg", "/sw nation", "refund", Translation.of("nation_help_11")));
+			sender.sendMessage(ChatTools.formatCommand("Eg", "/sw hud", "[town]", ""));
 		}
 		return true;
 	}
@@ -49,9 +59,14 @@ public class SiegeWarCommand implements CommandExecutor, TabCompleter {
 		case "nation":
 			parseSiegeWarNationCommand(player, StringMgmt.remFirstArg(args));
 			break;
+		case "focus":
+		case "hud":
+			parseSiegeWarHudCommand(player, StringMgmt.remFirstArg(args));
+			break;
 		default:
 			player.sendMessage(ChatTools.formatTitle("/siegewar"));
 			player.sendMessage(ChatTools.formatCommand("Eg", "/sw nation", "refund", Translation.of("nation_help_11")));
+			player.sendMessage(ChatTools.formatCommand("Eg", "/sw hud", "[town]", ""));
 		}
 	}
 
@@ -67,4 +82,26 @@ public class SiegeWarCommand implements CommandExecutor, TabCompleter {
 		}
 	}
 
+	private void parseSiegeWarHudCommand(Player player, String[] args) {
+		try {
+			if (args.length == 0) {
+				player.sendMessage(ChatTools.formatTitle("/siegewar"));
+				player.sendMessage(ChatTools.formatCommand("Eg", "/sw hud", "[town]", ""));
+			} else {
+				List<String> townsBeingSieged = new ArrayList<String>(SiegeController.getSiegedTownNames());
+				Town town = TownyUniverse.getInstance().getTown(args[0]);
+				if (town == null) 
+					throw new TownyException(Translation.of("msg_err_town_not_registered", args[0]));
+
+				if (!townsBeingSieged.contains(town.getName())) {
+					Messaging.sendErrorMsg(player, Translation.of("msg_err_not_being_sieged", town.getName()));
+					return;
+				}
+
+				SiegeWar.getSiegeHUDManager().toggleWarHud(player, SiegeController.getSiege(town));
+			}
+		} catch (Exception e) {
+			player.sendMessage(e.getMessage());
+		}
+	}
 }
