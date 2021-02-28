c799de70b3c1427e4b3b13348691f6344c3a3e72
goosius1 <=>
2021-02-28 10:48:24 +0000

    comment update

--- src/main/java/com/gmail/goosius/siegewar/listeners/SiegeWarTownEventListener.java
+++ src/main/java/com/gmail/goosius/siegewar/listeners/SiegeWarTownEventListener.java
@@ -313,7 +313,7 @@ public class SiegeWarTownEventListener implements Listener {
 						// > Attacker: Land of Empire (Nation)
 						out.add(Translation.of("status_town_siege_status_besieger", siege.getAttackingNation().getFormattedName()));
 
-						// > Points: +530
+						// > Balance: +530
 						int pointsInt = siege.getSiegeBalance();
 						String pointsString = pointsInt > 0 ? "+" + pointsInt : "" + pointsInt;
 						out.add(Translation.of("status_town_siege_status_siege_balance", pointsString));
