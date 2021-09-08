6910ed8635881c943455b886ac34ec26c8462fe0
goosius1 <=>
2021-09-08 10:32:05 +0100

    bat reporter bug fix

--- src/main/java/com/gmail/goosius/siegewar/utils/SiegeWarBattlefieldObserverUtil.java
+++ src/main/java/com/gmail/goosius/siegewar/utils/SiegeWarBattlefieldObserverUtil.java
@@ -20,6 +20,7 @@ public class SiegeWarBattlefieldObserverUtil {
      */
     public static void evaluateBattlefieldObserversInSiegezones() {
         List<ItemStack> itemsToDrop = new ArrayList<>();
+        List<ItemStack> itemsToKeep = new ArrayList<>();        
         List<Double> xDelta = new ArrayList<>();
         List<Double> yDelta = new ArrayList<>();
         List<Double> zDelta = new ArrayList<>();
@@ -40,18 +41,22 @@ public class SiegeWarBattlefieldObserverUtil {
 
                 //Identify non-tool items
                 itemsToDrop.clear();
+                itemsToKeep.clear();
                 xDelta.clear();
                 yDelta.clear();
                 zDelta.clear();;
                 for(ItemStack itemStack: player.getInventory().getContents()) {
-                    if(itemStack != null
-                            && !itemStack.getType().toString().endsWith("AXE")
-                            && !itemStack.getType().toString().endsWith("SHOVEL")
-                            && !itemStack.getType().toString().endsWith("SHEARS")) {
-                        itemsToDrop.add(itemStack);
-                        xDelta.add((Math.random() * 10) - 5);
-                        yDelta.add((Math.random() * 10) + 5);                        
-                        zDelta.add((Math.random() * 10) - 5);                  
+                    if(itemStack != null) {
+                        if (itemStack.getType().toString().endsWith("AXE")
+                            || itemStack.getType().toString().endsWith("SHOVEL")
+                            || itemStack.getType().toString().endsWith("SHEARS")) {
+                                itemsToKeep.add(itemStack);
+                        } else {
+                            itemsToDrop.add(itemStack);
+                            xDelta.add((Math.random() * 10) - 5);
+                            yDelta.add((Math.random() * 10) + 5);                        
+                            zDelta.add((Math.random() * 10) - 5);                                              
+                        }
                     }
                 }
 
@@ -59,14 +64,19 @@ public class SiegeWarBattlefieldObserverUtil {
                     //Drop items and scatter them around
                     Towny.getPlugin().getServer().getScheduler().runTask(Towny.getPlugin(), new Runnable() {
                         public void run() {
+                            //Clear inventory (We do not use remove() here, because unfortunately it only works on the storage contents)
+                            player.getInventory().clear();
+                            //Add any kept items back into inventory
+                            for(ItemStack itemStack: itemsToKeep) {
+                                player.getInventory().addItem(itemStack);
+                            }
+                            //Drop items on ground near player
                             for(int i = 0; i < itemsToDrop.size(); i++) {                            
                                 player.getWorld().dropItemNaturally(
                                     player.getLocation().add(xDelta.get(i), yDelta.get(i), zDelta.get(i)), 
                                     itemsToDrop.get(i));
                             }
-                            player.getInventory().removeItem(itemsToDrop.toArray(new ItemStack[0]));
-                        }
-                       
+                        }                       
                     });                   
                     //Notify player
                     player.sendMessage(Translation.of("plugin_prefix") + Translation.of("msg_you_cannot_carry_items_in_siegezones"));
