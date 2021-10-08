bf79b7e66049a7093fd2247abc362e185942db43
Haxx0r-4559 <78245071+Haxx0r-4559@users.noreply.github.com>
2021-10-08 18:23:53 -0400

    Correct wildcard import

--- src/main/java/com/gmail/goosius/siegewar/command/SiegeWarAdminCommand.java
+++ src/main/java/com/gmail/goosius/siegewar/command/SiegeWarAdminCommand.java
@@ -25,7 +25,9 @@ import com.palmergames.bukkit.towny.utils.NameUtil;
 import com.palmergames.bukkit.util.ChatTools;
 import com.palmergames.util.StringMgmt;
 import com.palmergames.util.TimeMgmt;
-import org.bukkit.command.*;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
