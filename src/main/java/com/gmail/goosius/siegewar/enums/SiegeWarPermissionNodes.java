6091eaf138544d4e3ded171112f844dcf0ed3e1e
Llm Dl <LlmDlio@gmail.com>
2021-10-08 19:28:19 -0500

    Add /sw nextsession
    - Shows a player how long until the next battlesession.
    - Closes #366.

--- src/main/java/com/gmail/goosius/siegewar/enums/SiegeWarPermissionNodes.java
+++ src/main/java/com/gmail/goosius/siegewar/enums/SiegeWarPermissionNodes.java
@@ -58,6 +58,7 @@ public enum SiegeWarPermissionNodes {
 		SIEGEWAR_COMMAND_SIEGEWAR_HUD("siegewar.command.siegewar.hud"),
 		SIEGEWAR_COMMAND_SIEGEWAR_GUIDE("siegewar.command.siegewar.guide"),
 		SIEGEWAR_COMMAND_SIEGEWAR_PREFERENCE("siegewar.command.siegewar.preference"),
+		SIEGEWAR_COMMAND_SIEGEWAR_NEXTSESSION("siegewar.command.siegewar.nextsession"),
 
 	// ----- Admin Command Nodes -----
 	SIEGEWAR_COMMAND_SIEGEWARADMIN("siegewar.command.siegewaradmin.*"),
