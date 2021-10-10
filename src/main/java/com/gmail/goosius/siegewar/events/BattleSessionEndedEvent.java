65a881ce47257d3c8fb2c6270f4238dc0c6cbe0b
Llm Dl <LlmDlio@gmail.com>
2021-10-10 06:13:15 -0500

    Fixes new event thrown async.

--- src/main/java/com/gmail/goosius/siegewar/events/BattleSessionEndedEvent.java
+++ src/main/java/com/gmail/goosius/siegewar/events/BattleSessionEndedEvent.java
@@ -1,5 +1,6 @@
 package com.gmail.goosius.siegewar.events;
 
+import org.bukkit.Bukkit;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
@@ -9,6 +10,7 @@ public class BattleSessionEndedEvent extends Event {
 	private static final HandlerList handlers = new HandlerList();
 
 	public BattleSessionEndedEvent() {
+		super(!Bukkit.getServer().isPrimaryThread());
 	}
 
 	@NotNull
