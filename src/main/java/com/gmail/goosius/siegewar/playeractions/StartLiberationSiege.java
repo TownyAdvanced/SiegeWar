5d29e77b4fc1fcaf5ba1b784e02e3adbcf8d2779
goosius1 <=>
2022-02-22 23:58:02 +0000

    fix for wall breaching in lib sieges

--- src/main/java/com/gmail/goosius/siegewar/playeractions/StartLiberationSiege.java
+++ src/main/java/com/gmail/goosius/siegewar/playeractions/StartLiberationSiege.java
@@ -58,12 +58,12 @@ public class StartLiberationSiege {
         if (!TownyUniverse.getInstance().getPermissionSource().testPermission(player, SiegeWarPermissionNodes.getPermissionNodeToStartSiege(SiegeType.LIBERATION)))
             throw new TownyException(Translation.of("msg_err_action_disable"));
 
-        Nation occupierNationOfTown = TownOccupationController.getTownOccupier(targetTown);
-        if (!nationOfSiegeStarter.hasEnemy(occupierNationOfTown))
+        Nation occupierNation = TownOccupationController.getTownOccupier(targetTown);
+        if (!nationOfSiegeStarter.hasEnemy(occupierNation))
             throw new TownyException(Translation.of("msg_err_siege_war_cannot_attack_occupied_town_non_enemy_nation"));
 
         Nation naturalNationOfTown = targetTown.getNationOrNull();
-        if(!naturalNationOfTown.hasMutualAlly(nationOfSiegeStarter))
+        if(naturalNationOfTown != null && !naturalNationOfTown.hasMutualAlly(nationOfSiegeStarter))
             throw new TownyException(Translation.of("msg_err_siege_war_cannot_start_liberation_siege_at_unallied_town"));
 
         if (TownySettings.getNationRequiresProximity() > 0) {
@@ -79,7 +79,7 @@ public class StartLiberationSiege {
             }
         }
 
-		SiegeCamp camp = new SiegeCamp(player, bannerBlock, SiegeType.LIBERATION, targetTown, nationOfSiegeStarter, occupierNationOfTown, townOfSiegeStarter, townBlock);
+		SiegeCamp camp = new SiegeCamp(player, bannerBlock, SiegeType.LIBERATION, targetTown, nationOfSiegeStarter, occupierNation, townOfSiegeStarter, townBlock);
 
 		PreSiegeCampEvent event = new PreSiegeCampEvent(camp);
 		Bukkit.getPluginManager().callEvent(event);
