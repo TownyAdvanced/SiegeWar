b975437c2af7281c6d89899f4a7aee1e44097959
Llm Dl <LlmDlio@gmail.com>
2022-03-04 14:50:27 -0600

    Undo diff in TownyEventListener.

--- src/main/java/com/gmail/goosius/siegewar/listeners/SiegeWarTownyEventListener.java
+++ src/main/java/com/gmail/goosius/siegewar/listeners/SiegeWarTownyEventListener.java
@@ -45,22 +45,22 @@ public class SiegeWarTownyEventListener implements Listener {
 
 	@SuppressWarnings("unused")
 	private final SiegeWar plugin;
-
+	
 	public SiegeWarTownyEventListener(SiegeWar instance) {
 
 		plugin = instance;
 	}
-
-	/*
-	 * Siegewar has to be conscious of when Towny has loaded the Towny database.
-	 */
-	@EventHandler
-	public void onTownyDatabaseLoad(TownyLoadedDatabaseEvent event) {
-		SiegeWar.info("Towny database reload detected, reloading sieges...");
-		SiegeController.loadAll();
-		TownOccupationController.loadAll();
-	}
-
+	
+	   /*
+     * Siegewar has to be conscious of when Towny has loaded the Towny database.
+     */
+    @EventHandler
+    public void onTownyDatabaseLoad(TownyLoadedDatabaseEvent event) {
+    	SiegeWar.info("Towny database reload detected, reloading sieges...");
+        SiegeController.loadAll();
+        TownOccupationController.loadAll();
+    }
+    
 	/*
 	 * When Towny is reloading the languages, make sure we're re-injecting our language strings. 
 	 */
@@ -76,136 +76,131 @@ public class SiegeWarTownyEventListener implements Listener {
 			for (Map.Entry<String, String> map : translations.get(language).entrySet())
 				event.addTranslation(language, map.getKey(), map.getValue());
 	}
+	
+    /*
+     * Update town peacefulness counters.
+     */
+    @EventHandler
+    public void onNewDay(PreNewDayEvent event) {
+        if (SiegeWarSettings.getWarSiegeEnabled() && SiegeWarSettings.getWarCommonPeacefulTownsEnabled()) {
+            TownPeacefulnessUtil.updateTownPeacefulnessCounters();
+        }
+    }
+    
+    /*
+     * On NewHours SW makes some calculations.
+     */
+    @EventHandler
+    public void onNewHour(NewHourEvent event) {
+        if(SiegeWarSettings.getWarSiegeEnabled()) {
+            SiegeWarImmunityUtil.evaluateExpiredImmunities();
+        }
+    }
 
-	/*
-	 * Update town peacefulness counters.
-	 */
-	@EventHandler
-	public void onNewDay(PreNewDayEvent event) {
-		if (SiegeWarSettings.getWarSiegeEnabled() && SiegeWarSettings.getWarCommonPeacefulTownsEnabled()) {
-			TownPeacefulnessUtil.updateTownPeacefulnessCounters();
-		}
-	}
+    /*
+     * On each ShortTime period, SW makes some calcuations.
+     */
+    @EventHandler
+    public void onShortTime(NewShortTimeEvent event) {
+        if (SiegeWarSettings.getWarSiegeEnabled()) {
+            SiegeWarTimerTaskController.punishPeacefulPlayersInActiveSiegeZones();
+            SiegeWarTimerTaskController.evaluateBattleSessions();
+            SiegeWarTimerTaskController.evaluateBannerControl();
+            SiegeWarTimerTaskController.evaluateWallBreaching();         
+            SiegeWarTimerTaskController.evaluateMapHiding();
+            SiegeWarTimerTaskController.evaluateTimedSiegeOutcomes();
+            SiegeWarTimerTaskController.punishNonSiegeParticipantsInSiegeZones();
+            SiegeHUDManager.updateHUDs();
+            SiegeWarTimerTaskController.evaluateBeacons();
+            SiegeWarTimerTaskController.evaluateBattlefieldReporters();
+        }
+    }
 
-	/*
-	 * On NewHours SW makes some calculations.
-	 */
-	@EventHandler
-	public void onNewHour(NewHourEvent event) {
-		if (SiegeWarSettings.getWarSiegeEnabled()) {
-			SiegeWarImmunityUtil.evaluateExpiredImmunities();
-		}
-	}
+    /**
+     * Process block explosion events coming from Towny 
+     *
+     * @param event the TownyExplodingBlocksEvent event
+     * @throws TownyException if something is misconfigured
+     */
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onBlockExploding(TownyExplodingBlocksEvent event) throws TownyException {
+        if(!SiegeWarSettings.getWarSiegeEnabled())
+            return;
+        if (event.getEntity() != null && !TownyAPI.getInstance().getTownyWorld(event.getEntity().getWorld()).isWarAllowed())
+            return;    
+        List<Block> filteredExplodeList = event.getTownyFilteredBlockList();
+        filteredExplodeList = CannonsIntegration.filterExplodeListByCannonEffects(filteredExplodeList, event);
+        filteredExplodeList = filterExplodeListBySiegeBannerProtection(filteredExplodeList);
+        filteredExplodeList = filterExplodeListByTrapWarfareMitigation(filteredExplodeList);
+        event.setBlockList(filteredExplodeList);
+    }
 
-	/*
-	 * On each ShortTime period, SW makes some calcuations.
-	 */
-	@EventHandler
-	public void onShortTime(NewShortTimeEvent event) {
-		if (SiegeWarSettings.getWarSiegeEnabled()) {
-			SiegeWarTimerTaskController.punishPeacefulPlayersInActiveSiegeZones();
-			SiegeWarTimerTaskController.evaluateBattleSessions();
-			SiegeWarTimerTaskController.evaluateBannerControl();
-			SiegeWarTimerTaskController.evaluateWallBreaching();
-			SiegeWarTimerTaskController.evaluateMapHiding();
-			SiegeWarTimerTaskController.evaluateTimedSiegeOutcomes();
-			SiegeWarTimerTaskController.punishNonSiegeParticipantsInSiegeZones();
-			SiegeHUDManager.updateHUDs();
-			SiegeWarTimerTaskController.evaluateBeacons();
-			SiegeWarTimerTaskController.evaluateBattlefieldReporters();
-		}
-	}
+    /**
+     * Filter a given explode list by siege banner protection
+     * @param givenExplodeList given list of exploding blocks
+     *
+     * @return filtered list
+     */
+    private static List<Block> filterExplodeListBySiegeBannerProtection(List<Block> givenExplodeList) {       
+        List<Block> filteredList = new ArrayList<>(givenExplodeList);
+        for(Block block: givenExplodeList) {
+            if (SiegeWarBlockUtil.isBlockNearAnActiveSiegeBanner(block)) {
+                //Remove block from final explode list
+                filteredList.remove(block);
+            } 
+        }
+        return filteredList;
+    }
 
-	/**
-	 * Process block explosion events coming from Towny
-	 *
-	 * @param event the TownyExplodingBlocksEvent event
-	 * @throws TownyException if something is misconfigured
-	 */
-	@EventHandler(priority = EventPriority.HIGH)
-	public void onBlockExploding(TownyExplodingBlocksEvent event) throws TownyException {
-		if (!SiegeWarSettings.getWarSiegeEnabled())
-			return;
-		if (event.getEntity() != null
-				&& !TownyAPI.getInstance().getTownyWorld(event.getEntity().getWorld()).isWarAllowed())
-			return;
-		List<Block> filteredExplodeList = event.getTownyFilteredBlockList();
-		filteredExplodeList = CannonsIntegration.filterExplodeListByCannonEffects(filteredExplodeList, event);
-		filteredExplodeList = filterExplodeListBySiegeBannerProtection(filteredExplodeList);
-		filteredExplodeList = filterExplodeListByTrapWarfareMitigation(filteredExplodeList);
-		event.setBlockList(filteredExplodeList);
-	}
+    /**
+     * Filter a given explode list by trap warfare mitigation
+     * @param givenExplodeList given list of exploding blocks
+     *
+     * @return filtered list
+     */
+    private static List<Block> filterExplodeListByTrapWarfareMitigation(List<Block> givenExplodeList) {       
+        if(!SiegeWarSettings.isTrapWarfareMitigationEnabled())
+            return givenExplodeList;
 
-	/**
-	 * Filter a given explode list by siege banner protection
-	 * 
-	 * @param givenExplodeList given list of exploding blocks
-	 *
-	 * @return filtered list
-	 */
-	private static List<Block> filterExplodeListBySiegeBannerProtection(List<Block> givenExplodeList) {
-		List<Block> filteredList = new ArrayList<>(givenExplodeList);
-		for (Block block : givenExplodeList) {
-			if (SiegeWarBlockUtil.isBlockNearAnActiveSiegeBanner(block)) {
-				// Remove block from final explode list
-				filteredList.remove(block);
-			}
-		}
-		return filteredList;
-	}
+        List<Block> filteredList = new ArrayList<>(givenExplodeList);
+        for(Block block: givenExplodeList) {
+            if (SiegeWarDistanceUtil.isLocationInActiveTimedPointZoneAndBelowSiegeBannerAltitude(block.getLocation())) {
+                //Remove block from final explode list
+                filteredList.remove(block);
+            } 
+        }
+        return filteredList;
+    }
 
-	/**
-	 * Filter a given explode list by trap warfare mitigation
-	 * 
-	 * @param givenExplodeList given list of exploding blocks
-	 *
-	 * @return filtered list
-	 */
-	private static List<Block> filterExplodeListByTrapWarfareMitigation(List<Block> givenExplodeList) {
-		if (!SiegeWarSettings.isTrapWarfareMitigationEnabled())
-			return givenExplodeList;
-
-		List<Block> filteredList = new ArrayList<>(givenExplodeList);
-		for (Block block : givenExplodeList) {
-			if (SiegeWarDistanceUtil.isLocationInActiveTimedPointZoneAndBelowSiegeBannerAltitude(block.getLocation())) {
-				// Remove block from final explode list
-				filteredList.remove(block);
-			}
-		}
-		return filteredList;
-	}
-
-	/**
-	 * During a siege, players in the town are not protected from explosion damage.
-	 * (a related effect to how PVP protection is forced off)
-	 *
-	 * @param event the TownyExplosionDamagesEntityEvent event
-	 */
-	@EventHandler(priority = EventPriority.HIGH)
-	public void onExplosionDamageEntity(TownyExplosionDamagesEntityEvent event) {
-		if (!event.isCancelled())
-			return;
-		if (!SiegeWarSettings.getWarSiegeEnabled())
-			return;
-		if (!TownyAPI.getInstance().getTownyWorld(event.getEntity().getWorld()).isWarAllowed())
-			return;
-		if (!(event.getEntity() instanceof Player))
-			return;
-		if (event.getTown() == null)
-			return;
-		if (SiegeController.hasActiveSiege(event.getTown()))
-			event.setCancelled(false);
-	}
-
-	/**
-	 * Prevent an outlaw being teleported away if the town they are outlawed in has
-	 * an active siege.
-	 * 
-	 * @param event OutlawTeleportEvent thrown by Towny.
-	 */
-	@EventHandler
-	public void onOutlawTeleportEvent(OutlawTeleportEvent event) {
-		if (SiegeWarSettings.getWarSiegeEnabled() && SiegeController.hasActiveSiege(event.getTown()))
-			event.setCancelled(true);
-	}
+    /**
+     * During a siege, players in the town are not protected from explosion damage.
+     * (a related effect to how PVP protection is forced off)
+     *
+     * @param event the TownyExplosionDamagesEntityEvent event
+     */
+    @EventHandler(priority = EventPriority.HIGH)
+    public void onExplosionDamageEntity(TownyExplosionDamagesEntityEvent event) {
+        if (!event.isCancelled())
+            return;
+        if(!SiegeWarSettings.getWarSiegeEnabled())
+            return;
+        if (!TownyAPI.getInstance().getTownyWorld(event.getEntity().getWorld()).isWarAllowed())
+            return;
+        if(!(event.getEntity() instanceof Player))
+            return;
+        if(event.getTown() == null)
+            return;
+        if(SiegeController.hasActiveSiege(event.getTown()))
+            event.setCancelled(false);
+    }
+    
+    /**
+     * Prevent an outlaw being teleported away if the town they are outlawed in has an active siege.
+     * @param event OutlawTeleportEvent thrown by Towny.
+     */
+    @EventHandler
+    public void onOutlawTeleportEvent(OutlawTeleportEvent event) {
+    	if (SiegeWarSettings.getWarSiegeEnabled() && SiegeController.hasActiveSiege(event.getTown())) 
+    		event.setCancelled(true);
+    }
 }
