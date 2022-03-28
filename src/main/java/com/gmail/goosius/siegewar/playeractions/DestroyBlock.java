fc3ffb2bf580800c5a91af344618c9f1972b0c94
goosius1 <=>
2022-03-28 13:11:17 +0100

    Cleanups

--- src/main/java/com/gmail/goosius/siegewar/playeractions/DestroyBlock.java
+++ src/main/java/com/gmail/goosius/siegewar/playeractions/DestroyBlock.java
@@ -57,7 +57,7 @@ public class DestroyBlock {
         //Trap warfare block protection
         if(SiegeWarSettings.isTrapWarfareMitigationEnabled()
                 && SiegeWarDistanceUtil.isLocationInSiegeZoneWildernessAndBelowSiegeBannerAltitude(event.getBlock().getLocation())) {
-            event.getPlayer().spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ChatColor.DARK_RED + translator.of("msg_err_cannot_alter_blocks_below_banner_in_timed_point_zone")));
+            event.getPlayer().spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(ChatColor.DARK_RED + translator.of("msg_err_cannot_alter_blocks_below_banner_in_siege_zone")));
             event.setCancelled(true);
             return;
         }
