c1073dab2343ad671830e7ee2d1d42c88bc426d5
goosius1 <=>
2022-03-30 13:04:40 +0100

    Changed name to auto-enable

--- src/main/java/com/gmail/goosius/siegewar/settings/ConfigNodes.java
+++ src/main/java/com/gmail/goosius/siegewar/settings/ConfigNodes.java
@@ -900,7 +900,7 @@ public enum ConfigNodes {
 			"# If this value is true, then a town under occupation cannot unclaim.",
 			"#  This setting is recommended, to avoid occupation escape exploits."),
 	PUNISH_NON_SIEGE_PARTICIPANTS_IN_SIEGE_ZONE(
-			"punish_non_siege_participants_in_siege_zone",
+			"punish_non_siege_participants_in_siege_zones",
 			"",
 			"",
 			"",
@@ -911,7 +911,7 @@ public enum ConfigNodes {
 			"############################################################",
 			""),
 	ENABLE_SICKNESS(
-			"punish_non_siege_participants_in_siege_zone.enable_sickness",
+			"punish_non_siege_participants_in_siege_zones.enable_sickness",
 			"true",
 			"",
 			"# If true, players that are not participating in a siege will receive war sickness",
@@ -920,10 +920,10 @@ public enum ConfigNodes {
 			"# Special war sickness is only given if a non-participant is at his town that happened to be in a siege zone",
 			"#   - Effects: Weakness V",
 			"# Full sickness is given to all players that are not allied to either side, do not have a military rank, or is neutral, and are not in their own town.",
-			"#   - Effects: Nausea V, Poison V, Weakness V, Slowness III, Mining Fatigue III"
+			"#   - Effects: Health 1, Nausea V, Poison V, Weakness V, Slowness III, Mining Fatigue III"
 	),
 	SECONDS_BEFORE_SICKNESS(
-			"punish_non_siege_participants_in_siege_zone.seconds_warning",
+			"punish_non_siege_participants_in_siege_zones.seconds_warning",
 			"20",
 			"",
 			"# This is how many seconds a player has to leave the siege zone before he gets war sickness",
