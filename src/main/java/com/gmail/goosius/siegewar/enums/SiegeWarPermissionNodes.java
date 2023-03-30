64588af806cb93d6c1d3424ed0961ba91898e707
Goosius1 <goosius1@gmail.com>
2023-03-30 14:26:31 +0100

    Removed sw guide

--- src/main/java/com/gmail/goosius/siegewar/enums/SiegeWarPermissionNodes.java
+++ src/main/java/com/gmail/goosius/siegewar/enums/SiegeWarPermissionNodes.java
@@ -42,7 +42,6 @@ public enum SiegeWarPermissionNodes {
 			SIEGEWAR_COMMAND_SIEGEWAR_NATION_CLAIMREFUND("siegewar.command.siegewar.nation.claimrefund"),
 		SIEGEWAR_COMMAND_SIEGEWAR_COLLECT("siegewar.command.siegewar.collect"),
 		SIEGEWAR_COMMAND_SIEGEWAR_HUD("siegewar.command.siegewar.hud"),
-		SIEGEWAR_COMMAND_SIEGEWAR_GUIDE("siegewar.command.siegewar.guide"),
 		SIEGEWAR_COMMAND_SIEGEWAR_PREFERENCE("siegewar.command.siegewar.preference"),
 		SIEGEWAR_COMMAND_SIEGEWAR_NEXTSESSION("siegewar.command.siegewar.nextsession"),
 
