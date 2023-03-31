1a07e51653b574ca8c412a6e2a7e7a0c2fd767b6
Goosius1 <goosius1@gmail.com>
2023-03-31 14:45:10 +0100

    cr cleanup

--- src/main/java/com/gmail/goosius/siegewar/listeners/SiegeWarStatusScreenListener.java
+++ src/main/java/com/gmail/goosius/siegewar/listeners/SiegeWarStatusScreenListener.java
@@ -147,7 +147,7 @@ public class SiegeWarStatusScreenListener implements Listener {
 					event.getStatusScreen().addComponentOf("siegeWarOccupationTax", comp);
 				}
 			}
-
+			
 			if (TownMetaDataController.hasPlunderDebt(town)) {
 				int days = TownMetaDataController.getPlunderDebtDays(town);
 				double amount = TownMetaDataController.getDailyPlunderDebt(town);
