a34399dc5238dfc84663b13c89b1de4ec6635bc2
Goosius1 <goosius1@gmail.com>
2023-04-01 01:25:50 +0100

    formatting

--- src/main/java/com/gmail/goosius/siegewar/listeners/SiegeWarNationEventListener.java
+++ src/main/java/com/gmail/goosius/siegewar/listeners/SiegeWarNationEventListener.java
@@ -49,6 +49,7 @@ public class SiegeWarNationEventListener implements Listener {
 				}
 			}
 		}
+
 	}
 	
 	/*
--- src/main/java/com/gmail/goosius/siegewar/utils/DataCleanupUtil.java
+++ src/main/java/com/gmail/goosius/siegewar/utils/DataCleanupUtil.java
@@ -139,4 +139,5 @@ public class DataCleanupUtil {
             NationMetaDataController.deleteLegacyMetadata(nation);
         }
     }
+
 }
