cc0a9c4d55c11b853946dedf80222cbb9c6e23fa
LlmDl <LlmDlio@gmail.com>
2024-06-22 12:59:01 -0500

    Use Towny's keepinv event to provide better compatiblity with other (#845)
    plugins.
    
    While maintaining SW having the final say for inventories in SiegeZones.

--- src/main/java/com/gmail/goosius/siegewar/utils/SiegeWarInventoryUtil.java
+++ src/main/java/com/gmail/goosius/siegewar/utils/SiegeWarInventoryUtil.java
@@ -3,18 +3,19 @@ package com.gmail.goosius.siegewar.utils;
 import com.gmail.goosius.siegewar.Messaging;
 import com.gmail.goosius.siegewar.settings.SiegeWarSettings;
 import com.palmergames.bukkit.towny.object.Translatable;
-import org.bukkit.event.entity.PlayerDeathEvent;
+
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.Damageable;
 
 public class SiegeWarInventoryUtil {
 
-    public static void degradeInventory(PlayerDeathEvent playerDeathEvent) {
+    public static void degradeInventory(Player player) {
         Damageable damageable;
         double maxDurability;
         int currentDurability, damageToInflict, newDurability, durabilityWarning;
         boolean closeToBreaking = false;
-        for (ItemStack itemStack : playerDeathEvent.getEntity().getInventory().getContents()) {
+        for (ItemStack itemStack : player.getInventory().getContents()) {
             if (itemStack != null && itemStack.getType().getMaxDurability() != 0 && !itemStack.getItemMeta().isUnbreakable()) {
                 damageable = ((Damageable) itemStack.getItemMeta());
                 maxDurability = itemStack.getType().getMaxDurability();
@@ -35,11 +36,6 @@ public class SiegeWarInventoryUtil {
             }
         }
         if (closeToBreaking) //One or more items are close to breaking, send warning.
-            Messaging.sendMsg(playerDeathEvent.getEntity(), Translatable.of("msg_inventory_degrade_warning"));
-    }
-
-    public static void keepInventory(PlayerDeathEvent playerDeathEvent) {
-        playerDeathEvent.setKeepInventory(true);
-        playerDeathEvent.getDrops().clear();
+            Messaging.sendMsg(player, Translatable.of("msg_inventory_degrade_warning"));
     }
 }
