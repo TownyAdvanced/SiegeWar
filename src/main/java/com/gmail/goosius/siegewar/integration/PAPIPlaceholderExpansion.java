11b868fecc0d9d57139964909c65badba1c536cd
LlmDl <llmdlio@gmail.com>
2026-02-06 09:51:16 -0600

    Add placeholders to new class.

--- src/main/java/com/gmail/goosius/siegewar/integration/PAPIPlaceholderExpansion.java
+++ src/main/java/com/gmail/goosius/siegewar/integration/PAPIPlaceholderExpansion.java
@@ -1,11 +1,21 @@
 package com.gmail.goosius.siegewar.integration;
 
 import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 import com.gmail.goosius.siegewar.SiegeWar;
-import com.palmergames.bukkit.towny.TownyUniverse;
-import com.palmergames.bukkit.towny.object.Resident;
+import com.gmail.goosius.siegewar.SiegeWarAPI;
+import com.gmail.goosius.siegewar.listeners.SiegeWarStatusScreenListener;
+import com.gmail.goosius.siegewar.metadata.TownMetaDataController;
+import com.gmail.goosius.siegewar.objects.BattleSession;
+import com.gmail.goosius.siegewar.objects.Siege;
+import com.gmail.goosius.siegewar.settings.SiegeWarSettings;
+import com.gmail.goosius.siegewar.utils.SiegeWarImmunityUtil;
+import com.palmergames.bukkit.towny.TownyAPI;
+import com.palmergames.bukkit.towny.object.Town;
+import com.palmergames.bukkit.towny.object.Translator;
+import com.palmergames.util.TimeMgmt;
 
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import net.md_5.bungee.api.ChatColor;
@@ -33,16 +43,70 @@ public class PAPIPlaceholderExpansion extends PlaceholderExpansion {
 	}
 
 	private String getOfflinePlayerPlaceholder(OfflinePlayer player, String identifier) {
-		if (player == null && !identifier.startsWith("top_")) {
+		if (identifier.startsWith("player_") && (player == null || !player.isOnline())) {
 			return "";
 		}
 
-		Resident resident = TownyUniverse.getInstance().getResident(player.getUniqueId());
+		// All player-oriented placeholders pass through here. 
+		if (identifier.startsWith("player_"))
+			return getPlayerFocusedPlaceholder(player.getPlayer(), identifier);
 
-		if (resident == null)
-			return "";
-		
+		switch (identifier) {
+		case "siege_battlesession_time_remaining" -> { // %siegewar_siege_battlesession_time_remaining%
+			if (!SiegeWarAPI.isBattleSessionActive())
+				return "";
+			return BattleSession.getBattleSession().getFormattedTimeRemainingUntilBattleSessionEnds();
+		}
+		};
 		return "";
+	}
+
+	private String getPlayerFocusedPlaceholder(Player player, String identifier) {
+		Town town = TownyAPI.getInstance().getTown(player);
+		Siege siege = SiegeWarAPI.getSiegeOrNull(player);
+		Translator translator = Translator.locale(player);
+		String value = switch(identifier) {
+		case "player_is_sieged" -> String.valueOf(SiegeWarAPI.hasSiege(player)); // %siegewar_player_is_sieged%
+		case "player_immunity_timer" -> { // %siegewar_player_immunity_timer%
+			if (town == null)
+				yield "";
+			if (!SiegeWarImmunityUtil.isTownSiegeImmune(town))
+				yield "";
+			long immunity = TownMetaDataController.getSiegeImmunityEndTime(town);
+			if (System.currentTimeMillis() < immunity || immunity == -1l) {
+				String time = immunity == -1l ? translator.of("msg_permanent") : TimeMgmt.getFormattedTimeValue(immunity- System.currentTimeMillis());
+				yield translator.of("status_town_siege_immunity_timer", time);
+			}
+			yield "";
+		}
+		case "player_siege_attackers" -> { // %siegewar_player_siege_attackers%
+			if (siege == null)
+				yield "";
+			yield translator.of("status_town_siege_attacker", siege.getAttackerNameForDisplay());
+		}
+		case "player_siege_defenders" -> { // %siegewar_player_siege_defenders%
+			if (siege == null)
+				yield "";
+			yield translator.of("status_town_siege_defender", siege.getDefenderNameForDisplay());
+		}
+		case "player_siege_progress" -> { // %siegewar_player_siege_progress%
+			if (siege == null)
+				yield "";
+			yield translator.of("status_town_siege_status", SiegeWarStatusScreenListener.getStatusTownSiegeSummary(siege, translator));
+		}
+		case "player_siege_battle_sessions_remaining" -> { // %siegewar_player_siege_battle_sessions_remaining%
+			if (siege == null)
+				yield "";
+			yield switch (siege.getStatus()) {
+			case IN_PROGRESS, PENDING_ATTACKER_ABANDON, PENDING_DEFENDER_SURRENDER ->
+				translator.of("status_town_siege_progress", siege.getNumBattleSessionsCompleted(), SiegeWarSettings.getSiegeDurationBattleSessions());
+			default -> "";
+			};
+		}
+
+		default -> "";
+		};
+		return value;
 	}
 
 }
